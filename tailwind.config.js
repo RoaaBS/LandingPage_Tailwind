3af38c572ea2ec5fe19d5e03026d760630230f69
RoaaBS <s11924302@stu.najah.edu>
2024-12-31 19:55:34 +0200

    last uppdate

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+
   theme: {
     extend: {
       screens:{
@@ -17,5 +18,5 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  plugins: [ ],
 }
